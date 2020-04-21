b44dd790975f56367127564ca132f11a2ee29d7d
ftToti <ft.1rjn@gmail.com>
2020-04-22 03:49:24 +0900

    2-8確認問題答え合わせ前

---
+++ lesson7_2.rb
@@ -0,0 +1,33 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+a=gets
+puts "1回目の計算"
+puts "2つの値を入力してください"
+b=gets.to_i
+c=gets.to_i
+puts "計算結果を出力します"
+puts "b+c=#{b+c}"
+puts "b-c=#{b-c}"
+puts "b*c=#{b*c}"
+puts "b/c=#{b/c}"
+
+puts "2つ目の計算"
+puts "2つの値を入力してください"
+d=gets.to_i
+e=gets.to_i
+puts "計算結果を出力します"
+puts "d+e=#{d+e}"
+puts "d-e=#{d-e}"
+puts "d*e=#{d*e}"
+puts "d/e=#{d/e}"
+
+puts "3つ目の計算"
+puts "2つの値を入力してください"
+f=gets.to_i
+g=gets.to_i
+puts "計算結果を出力します"
+puts "f+g=#{f+g}"
+puts "f-g=#{f-g}"
+puts "f+g=#{f*g}"
+puts "f/g=#{f/g}"
+puts "計算を終了します"
