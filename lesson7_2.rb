e9cdc765d89ac275609b8cd226e9c618d54bf341
ftToti <ft.1rjn@gmail.com>
2020-04-22 04:51:15 +0900

    [Start 2-9確認問題]

--- lesson7_2.rb
+++ lesson7_2.rb
@@ -1,33 +1,28 @@
 puts "計算を始めます"
-puts "何回繰り返しますか？"
-a=gets
-puts "1回目の計算"
-puts "2つの値を入力してください"
-b=gets.to_i
-c=gets.to_i
-puts "計算結果を出力します"
-puts "b+c=#{b+c}"
-puts "b-c=#{b-c}"
-puts "b*c=#{b*c}"
-puts "b/c=#{b/c}"
+puts "何回計算を繰り返しますか？"
 
-puts "2つ目の計算"
-puts "2つの値を入力してください"
-d=gets.to_i
-e=gets.to_i
-puts "計算結果を出力します"
-puts "d+e=#{d+e}"
-puts "d-e=#{d-e}"
-puts "d*e=#{d*e}"
-puts "d/e=#{d/e}"
+input = gets.to_i
 
-puts "3つ目の計算"
-puts "2つの値を入力してください"
-f=gets.to_i
-g=gets.to_i
-puts "計算結果を出力します"
-puts "f+g=#{f+g}"
-puts "f-g=#{f-g}"
-puts "f+g=#{f*g}"
-puts "f/g=#{f/g}"
-puts "計算を終了します"
+i = 1
+
+while i <= input do
+
+	puts "#{i}回目の計算"
+	puts "2つの値を入力してください"
+
+	a = gets.to_i
+	b = gets.to_i
+
+	puts "a=#{a}"
+	puts "b=#{b}"
+
+	puts "計算結果を出力します"
+
+	puts "a+b=#{a+b}"
+	puts "a-b=#{a-b}"
+	puts "a*b=#{a*b}"
+	puts "a/b=#{a/b}"
+
+	i += 1
+
+end
